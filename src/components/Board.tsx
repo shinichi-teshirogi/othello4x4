58a99eeea7d4ca963c127d1b712b2a773bc5289a
shinichi-teshirogi <shinichi-teshirogi@eac-inc.co.jp>
2024-08-24 14:57:23 +0000

    tic-tac-toe 4x4

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -11,16 +11,25 @@ export const Board = () => {
                 <Cell text={cells[0]} onClick={() => cellClick(0)} />
                 <Cell text={cells[1]} onClick={() => cellClick(1)} />
                 <Cell text={cells[2]} onClick={() => cellClick(2)} />
+                <Cell text={cells[3]} onClick={() => cellClick(3)} />
             </Box>
             <Box>
-                <Cell text={cells[3]} onClick={() => cellClick(3)} />
                 <Cell text={cells[4]} onClick={() => cellClick(4)} />
                 <Cell text={cells[5]} onClick={() => cellClick(5)} />
-            </Box>
-            <Box>
                 <Cell text={cells[6]} onClick={() => cellClick(6)} />
                 <Cell text={cells[7]} onClick={() => cellClick(7)} />
+            </Box>
+            <Box>
                 <Cell text={cells[8]} onClick={() => cellClick(8)} />
+                <Cell text={cells[9]} onClick={() => cellClick(9)} />
+                <Cell text={cells[10]} onClick={() => cellClick(10)} />
+                <Cell text={cells[11]} onClick={() => cellClick(11)} />
+            </Box>
+            <Box>
+                <Cell text={cells[12]} onClick={() => cellClick(12)} />
+                <Cell text={cells[13]} onClick={() => cellClick(13)} />
+                <Cell text={cells[14]} onClick={() => cellClick(14)} />
+                <Cell text={cells[15]} onClick={() => cellClick(15)} />
             </Box>
         </Box>
     )
--- src/hooks/useGameController.ts
+++ src/hooks/useGameController.ts
@@ -3,7 +3,7 @@ import { CellText } from '../types/CellText'
 
 export const useGameController = () => {
     const [isFirst, setIsFirst] = useState(true)
-    const [cells, setCells] = useState<CellText[]>(Array(9).fill(' '))
+    const [cells, setCells] = useState<CellText[]>(Array(16).fill(' '))
     const cellClick = (i: number) => {
         if (isWin) return
         if (cells[i] !== ' ') return
@@ -15,20 +15,23 @@ export const useGameController = () => {
     }
     const nowPlayer: CellText = isFirst ? 'O' : 'X'
     const victoryConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [ 0, 1, 2, 3],
+        [ 4, 5, 6, 7],
+        [ 8, 9,10,11],
+        [12,13,14,15],
+        [ 0, 4, 8,12],
+        [ 1, 5, 9,13],
+        [ 2, 6,10,14],
+        [ 3, 7,11,15],
+        [ 0, 5,10,15],
+        [ 3, 6, 9,12],
     ]
     const isWin = victoryConditions.some((victoryCondition) => {
-        const [a, b, c] = victoryCondition
+        const [a, b, c, d] = victoryCondition
         if (cells[a] === ' ') return false
         if (cells[a] !== cells[b]) return false
         if (cells[a] !== cells[c]) return false
+        if (cells[a] !== cells[d]) return false
         return true
     })
     const winner: CellText = isFirst ? 'X' : 'O'
